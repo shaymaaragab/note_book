57e0a0ccee5c94cde75271da0afe8ce2cce310da
shaymaaragab <shaymaaragab123@gmail.com>
2018-07-04 20:43:55 +0200

    sadsadsa

---
+++ database/migrations/2016_09_02_113112_create_notes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->increments('id');
+
+
+            $table->integer('notebook_id')->unsigned();
+
+
+            $table->dropForeign(['notebook_id']);
+            $table->foreign('notebook_id')->references('id')->on('notebooks')->onDelete('cascade');
+            $table->string('title');
+            $table->text('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+}
